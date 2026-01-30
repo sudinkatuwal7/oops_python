2c90f708b1e6c0d2b326f1e278a53c44639ffe9b
Sudin Katuwal <sudcrack2024@gmail.com>
2026-01-30 00:18:22 -0600

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -8,26 +8,26 @@ class Scoreboard(Turtle):
         super().__init__()
         self.color("white")
         self.score = 0
-        with open("data.txt") as data:
-            self.high_score = int(data.read())
+        self.highscore = 0
         self.penup()
         self.goto(0,250)
         self.hideturtle()
         self.update_scoreboard()
-        
+
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score : {self.score}    High Score: {self.highscore}", align=ALIGNMENT, font=FONT)
 
     def reset(self):
-        if self.score > self.high_score:
-            self.high_score = self.score
-            with open("data.txt", mode="w") as data:
-                data.write(f"{self.high_score}")
-        self.score = 0
-        self.update_scoreboard()
+        if self.score > self.highscore:
+            self.highscore = self.score
+            self.score = 0
+            self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("Game khelna aayena bhai lai", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
