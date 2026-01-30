84b7c59586ad5b1629fd31749c087d9353970427
Sudin Katuwal <sudcrack2024@gmail.com>
2026-01-30 02:36:22 -0600

    Update snake.py

--- snake.py
+++ snake.py
@@ -13,14 +13,22 @@ class Snake:
         self.segments = []
         self.create_snake()
         self.head = self.segments[0]
-
+#   ------------------------------------------------
+#   functions to create snake
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+#   -------------------------------------------------
+    def extend(self):
+    #     add new segment to the snake
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
@@ -32,19 +40,29 @@ class Snake:
     def up(self):
         if self.head.heading() != DOWN:
              self.head.setheading(90)
+        # self.segments[0].sety(self.segments[0].ycor() + MOVE_DISTANCE)
 
     def down(self):
         if self.head.heading() != UP:
              self.head.setheading(270)
+        # self.segments[0].sety(self.segments[0].ycor() - MOVE_DISTANCE)
 
     def left(self):
         if self.head.heading() != RIGHT:
              self.head.setheading(180)
+        # self.segments[0].setx(self.segments[0].xcor() - MOVE_DISTANCE)
 
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(0)
+        # self.segments[0].setx(self.segments[0].xcor() + MOVE_DISTANCE)
 
+    def reset(self):
+        for segment in self.segments:
+            segment.goto(1000,1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
 
 
 # Good to know,
@@ -59,5 +77,6 @@ class Snake:
 
 # self.segments[0].sety(self.segments[0].ycor() - MOVE_DISTANCE)
 # Purpose: Move the snake head downward
+
 
 
